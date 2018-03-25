4144c5901759057509bc648be5396cd167b2b45e
Hawking <dragon.oug@gmail.com>
2018-03-25 23:26:48 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,6 +8,7 @@ before_action :authenticate_user!, only: [:new]
 	end
 	def create
 		@group = Group.new(group_params)
+		@group.user = current_user
 		if @group.save
 		   redirect_to groups_path
 	    else
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ApplicationRecord
+	blongs_to :user
 	validates :title, presence: true
 end
