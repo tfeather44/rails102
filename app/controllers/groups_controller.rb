a069ed749557c1ed488f299d535531cfd24e49c8
Hawking <dragon.oug@gmail.com>
2018-03-27 16:23:47 +0800

    when created a group user join it auomatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,6 +11,7 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 		@group = Group.new(group_params)
 		@group.user = current_user
 		if @group.save
+			current_user.join!(@group)
 		   redirect_to groups_path
 	    else
 	    	render :new
@@ -22,7 +23,6 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 	end
 	def edit
 		
-		end
 	end
 	def update
 		
@@ -37,6 +37,32 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 		@group.destroy
 		redirect_to groups_path, alert: "Group deleted"
 	end
+	def join
+    @group = Group.find(params[:id])
+  
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功！"
+    else
+      flash[:warning] = "你已经是本讨论版成员了！"
+    end
+  
+    redirect_to group_path(@group)
+    end
+  
+    def quit
+    @group = Group.find(params[:id])
+  
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论版！"
+    else
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+    end
+  
+    redirect_to group_path(@group)
+    end
+    
 
 	private
 
@@ -44,6 +70,7 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 		@group = Group.find(params[:id])
 		if current_user != @group.user
 			redirect_to root_path, alert: "You have no permission."
+		end
 	end
 
 	def group_params
